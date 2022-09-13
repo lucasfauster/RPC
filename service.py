3a47524db3622ea9e3ce7adeac63cab797d0cb4f
Lucas Fauster <lucasfauster@hotmail.com>
2022-09-13 11:55:57 -0300

    default

---
+++ service.py
@@ -0,0 +1,25 @@
+import rpyc
+from rpyc.utils.server import ThreadedServer
+
+
+class MyService(rpyc.Service):
+    def on_connect(self, conn):
+        # código que é executado quando uma conexão é iniciada, caso seja necessário
+        pass
+    
+    def on_disconnect(self, conn):
+        # código que é executado quando uma conexão é finalizada, caso seja necessário
+        pass
+    
+    def exposed_get_answer(self): # este é um método exposto
+        return 42
+
+    exposed_the_real_answer_though = 43 # este é um atributo exposto
+
+    def get_question(self): # este método não é exposto
+        return "Qual é a cor do cavalo branco de Napoleão?"
+    
+#Para iniciar o servidor
+if __name__ == "__main__":
+    t = ThreadedServer(MyService, port=8000)
+    t.start()
